038fecb3773b4e89a70287e50ae33045326abd74
Tim Lin <e0610k@gmail.com>
2022-07-07 15:56:06 +0800

    deploy

---
+++ assets/NotFound-542bbc9c.js
@@ -0,0 +1,1 @@
+import{u as t,g as e,o as s,c as a}from"./index-39e033da.js";const o={class:"mx-auto flex max-w-xl items-center justify-center p-4"},n={__name:"NotFound",setup(n){const u=t();return e((()=>{u.$patch((t=>{t.backRoute={name:"BooksList"}}))})),(t,e)=>(s(),a("div",o," 404 Not Found ! "))}};export{n as default};
